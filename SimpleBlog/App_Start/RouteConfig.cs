603a9b68d013a451e4b6a429dfda8e98744aae52
Tokunbo Quaye <toksquaye@yahoo.com>
2015-06-23 19:33:10 -0400

    Added basic routes and Admin Area

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,13 +12,25 @@ namespace SimpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+
+            //get namespace of postcontroller in main controller folder
+            var namespaces = new[] { typeof(PostsController).Namespace };
+            //routes to ignore. this is always there. dont delete
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            //remove default. hand write all my routes
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
+
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+            //Home - name of page
+            //"" - home page - dosn't need any slashes
+            // controller called is PostsController
+            // method in PostsController called is Index
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
         }
     }
 }
---
+++ SimpleBlog/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/
+
+        /*public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        public ActionResult Login()
+        {
+            return Content("Login");
+        }
+
+    }
+}
