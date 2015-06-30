84a69e9331b2eaa81d1ed390af9193d3b0986df4
Tokunbo Quaye <toksquaye@yahoo.com>
2015-06-29 23:40:15 -0400

    Added basic login form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SimpleBlog.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace SimpleBlog.Controllers
 {
@@ -16,10 +18,26 @@ namespace SimpleBlog.Controllers
             return View();
         }*/
 
+        /*default - get action*/
         public ActionResult Login()
         {
             //return Content("Login");
-            return View();
+            return View( new AuthLogin {});
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid) //if validation check fails
+                return View(form);  //give user form back
+
+            if(form.Username != "toks")
+            {
+                ModelState.AddModelError("Username", "Username or password is wrong");
+                return View(form);
+            }
+            return Content("The form is valid!");
+
         }
 
     }
---
+++ SimpleBlog/ViewModel/Auth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.ViewModel
+{
+    public class AuthLogin
+    {
+        //public string Test { get; set; }
+        [Required]
+        public string Username { get; set; }
+
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
