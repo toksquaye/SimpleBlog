d0d434ba23ef0d243c33e8201278f36968635704
Tokunbo Quaye <toksquaye@yahoo.com>
2015-07-01 16:49:58 -0400

    Created basic admin layout

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Infracstructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +8,12 @@ using System.Web.Mvc;
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")] //only a logged in admin can use controller
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         public ActionResult Index()
         {
-            return Content("USERS");
+            return View();
         }
     }
 }
