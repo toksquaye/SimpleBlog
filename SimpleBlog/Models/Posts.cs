0ed62585513e3faed0572cdd0531f3680ae855a8
Tokunbo Quaye <toksquaye@yahoo.com>
2015-07-09 15:18:28 -0400

    Created posts/tags models and migrations

---
+++ SimpleBlog/Models/Posts.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System.Collections.Generic;
+
+namespace SimpleBlog.Models
+{
+    public class Post
+    {
+        public virtual int Id { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual string Title { get; set; }
+        public virtual string Slug { get; set; }
+        public virtual string Content { get; set; }
+
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime? UpdatedAt { get; set; } //? denotes that is a nullable type
+        public virtual DateTime? DeletedAt { get; set; } //soft delete. admin sees deleted posts, but not user
+
+        public virtual IList<Tag> Tags { get; set; }
+        public virtual bool IsDeleted { get { return DeletedAt != null; } }
+    }
+
+    public class PostMap : ClassMapping<Post>
+    {
+        public PostMap()
+        {
+            Table("posts");
+            Id(x => x.Id, x => x.Generator(Generators.Identity));
+            ManyToOne(x => x.User, x =>
+                {
+                    x.Column("user_id");
+                    x.NotNullable(true);
+                });
+
+            Property(x => x.Title, x => x.NotNullable(true));
+            Property(x => x.Slug, x => x.NotNullable(true));
+            Property(x => x.Content, x => x.NotNullable(true));
+
+            Property(x => x.CreatedAt, x =>
+                {
+                    x.Column("created_at");
+                    x.NotNullable(true);
+                });
+
+            Property(x => x.UpdatedAt, x => x.Column("updated_dat"));
+            Property(x => x.DeletedAt, x => x.Column("deleted_at"));
+
+            Bag(x => x.Tags, x =>
+            {
+                x.Key(y => y.Column("post_id")); //key - entity we're mapping from, column - entity we're mapping to
+                x.Table("post_tags"); //pivot table
+            }, x => x.ManyToMany(y => y.Column("tag_id")));
+
+        }
+
+    }
+}
---
+++ SimpleBlog/Models/Tag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System.Collections.Generic;
+
+namespace SimpleBlog.Models
+{
+    public class Tag
+    {
+        public virtual int Id { get; set; }
+        public virtual string Slug { get; set; }
+        public virtual string Name { get; set; }
+
+        public virtual IList<Post> Posts { get; set; } //Posts and Tags have a manytomany relationship
+    }
+
+    public class TagMap : ClassMapping<Tag>
+    {
+        public TagMap()
+        {
+            Table("tags");
+
+            Id(x => x.Id, x => x.Generator(Generators.Identity));
+
+            Property(x => x.Name, x => x.NotNullable(true));
+            Property(x => x.Slug, x => x.NotNullable(true));
+
+            Bag(x => x.Posts, x =>
+                {
+                    x.Key(y => y.Column("tag_id")); //key - entity we're mapping from, column - entity we're mapping to
+                    x.Table("post_tags"); //pivot table
+                }, x => x.ManyToMany(y => y.Column("post_id")));
+        }
+    }
+}
