4d6e5ddde45c4a99ab57d4b0665e0752c793f703
Tokunbo Quaye <toksquaye@yahoo.com>
2015-07-10 10:04:13 -0400

    Created new and edit actions for the posts controller

--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using SimpleBlog.Infracstructure;
 using SimpleBlog.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleBlog.Areas.Admin.ViewModels
 {
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId {get; set;} //determines if its new form or edit form
+
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
     }
 }
